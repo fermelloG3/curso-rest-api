a761f57546bf038457655e6ef70b17df842db855
ferdemello <110197887+fermelloG3@users.noreply.github.com>
2024-02-17 07:32:39 -0300

    Create ApiErros Class

---
+++ CursoApiRest/src/main/java/io/github/fermelloG3/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,9 @@
+package io.github.fermelloG3.rest.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ApplicationControllerAdvice {
+
+
+}
